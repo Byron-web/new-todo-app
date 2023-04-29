94441fdeddc0c8b28411de6e00c7e395c6105a2a
Byron-web <byron.labuschagne@gmail.com>
2023-04-29 09:20:35 +0200

    refactor

--- client/src/components/todo/TodoList.js
+++ client/src/components/todo/TodoList.js
@@ -1,24 +1,59 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-  const todos = [
-    {
-      id: 1,
-      title: "Buy groceries",
-      description: "Milk, bread, eggs, cheese",
-    },
-  ];
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    fetch("/api/todo")
+      .then((response) => response.json())
+      .then((data) => setTodos(data));
+  }, []);
+
+  const handleCreate = (event) => {
+    event.preventDefault();
+    fetch("/api/todo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: newTodo, completed: false }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos([...todos, data]);
+        setNewTodo("");
+      });
+  };
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
 
   return (
     <div>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
-      <Button className="mt-3" variant="primary">
-        Create
-      </Button>{" "}
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo._id} todo={todo} />
+        ))}
+      </ul>
+      <Form onSubmit={handleCreate}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter title"
+            value={newTodo}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Create
+        </Button>{" "}
+      </Form>
     </div>
   );
 };
