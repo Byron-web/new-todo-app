1ef41b5c71ea4084eab4beab1a2ec7275d812362
Byron-web <byron.labuschagne@gmail.com>
2023-04-30 11:00:21 +0200

    todo refactor

--- client/src/components/todo/TodoItem.js
+++ client/src/components/todo/TodoItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
-const TodoItem = ({ title }) => {
+const TodoItem = ({ id, title }) => {
   return (
-    <Card>
-      <Card.Body className="p-4">
-        <Card.Title>{title}</Card.Title>
-        <Button variant="primary">Edit</Button>{" "}
-        <Button variant="danger">Delete</Button>{" "}
-      </Card.Body>
-    </Card>
+    <div key={id}>
+      <Card>
+        <Card.Body className="p-4">
+          <Card.Title>{title}</Card.Title>
+          <Button variant="primary">Edit</Button>{" "}
+          <Button variant="danger">Delete</Button>{" "}
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
